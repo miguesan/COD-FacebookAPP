b04c2f37e99531fdb0a9fa2d501409271ebdd240
Miguel Sanchez Blanco <msanchezblanco@danielcastelao.org>
2017-04-24 09:01:45 +0200

    Metodos creados

---
+++ src/facebook_contornos/Metodos.java
@@ -0,0 +1,41 @@
+
+package facebook_contornos;
+
+public class Metodos {
+    
+    public void cambEstado(){//Cambiar el estado usando permisos de visibilidad
+    
+    }
+    
+    public void darGusta(){ //Darle me gusta a un post
+        
+    }
+    
+    public void pubImagen(){//Post con imagen
+        
+    }
+    
+    public void lineaTiempo(){ //Busca un tema en la linea de tiempo
+        
+    }
+    
+    public void comentariosPublicacion(){ //Comentarios de una publicación
+        
+    }
+    
+    public void comentar(){ //Publicar un comentario
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
