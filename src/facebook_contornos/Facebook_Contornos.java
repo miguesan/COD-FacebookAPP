213a66ef102e36afb8c5252ffa77ecea1fc7db5f
Miguel Sanchez Blanco <msanchezblanco@danielcastelao.org>
2017-04-26 10:31:36 +0200

    Facebook

--- src/facebook_contornos/Facebook_Contornos.java
+++ src/facebook_contornos/Facebook_Contornos.java
@@ -1,39 +1,52 @@
 
 package facebook_contornos;
 
+import facebook4j.FacebookException;
+import java.net.MalformedURLException;
 import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Miguel
+ */
 
 public class Facebook_Contornos {
 
-    public static void main(String[] args) {
+    /**
+     * @param args the command line arguments
+     * @throws FacebookException
+     */
+    
+    public static void main(String[] args) throws FacebookException, MalformedURLException{
         
         Metodos met=new Metodos();
         
-        
-        
         int opciones;
         do{
-            opciones=Integer.parseInt(JOptionPane.showInputDialog("1- Cambiar Estado \n2- 'Me Gusta' un Post \n3- Publicar Imagen \n4- Buscar en TimeLine \n5- Comentar Publicacion \n6- Publicar Comentario/Estado \n0- Salir"));
+            opciones=Integer.parseInt(JOptionPane.showInputDialog("1- Cambiar Estado \n2- 'Me Gusta' un Post \n3- Publicar Imagen por URL \n4- Buscar en TimeLine/Post \n5- Hacer un Comentario \n6- Mostrar Biografia y Comentarios de Publicaciones \n7- Publicar Imagen \n0- Salir"));
                 switch(opciones){
-                    case 1:
-                        met.cambEstado();
+                    case 1: //Cambiar Estado 
+                        met.cambEstado(JOptionPane.showInputDialog("Introduce el nuevo Estado: "));
                         break;
-                    case 2:
+                    case 2: //'Me Gusta' un Post 
                         met.darGusta();
                         break;
-                    case 3:
-                        met.pubImagen();
+                    case 3: //Publicar Imagen por URL
+                        met.pubImagenURL();
                         break;
-                    case 4:
-                        met.lineaTiempo();
+                    case 4: //Buscar en TimeLine
+                        met.buscarPost();
                         break;
-                    case 5:
-                        met.comentariosPublicacion();
-                        break;
-                    case 6:
+                    case 5: //Comentar Publicacion 
                         met.comentar();
+                        break;  
+                    case 6: //Ver Biografia y Mostrar Comentarios Publicacion
+                        met.mirarBiografia();   
                         break;
-                    case 0:
+                    case 7: //Publicar Imagen
+                        met.subirImagen();
+                        break;
+                    case 0: //Salir del Programa
                         System.exit(0);
                         break;
                     default:
--- src/facebook_contornos/Metodos.java
+++ src/facebook_contornos/Metodos.java
@@ -1,41 +1,133 @@
 
 package facebook_contornos;
 
+import facebook4j.Facebook;
+import facebook4j.FacebookException;
+import facebook4j.FacebookFactory;
+import facebook4j.Post;
+import facebook4j.PostUpdate;
+import facebook4j.ResponseList;
+import facebook4j.conf.ConfigurationBuilder;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Miguel
+ */
+
 public class Metodos {
     
-    public void cambEstado(){//Cambiar el estado usando permisos de visibilidad
+    
+    
+    public Facebook facebook;
+    ResponseList<Post> results;
+
+    /**
+     * Introducir claves para que se conecte a tu cuenta de Facebook y puedas utilizar 
+     * este programa sin errores.
+     * NOTA: Los * representan donde tienes que introducir las claves.
+     */
+    
+    public Metodos() { //Constructor con las KEYs
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+        .setOAuthAppId("260029277794629")
+        .setOAuthAppSecret("3d03f2569b5b8b8d69a09a4a1b6716b4")
+        .setOAuthAccessToken("EAADsfsi78UUBAOYCcJXNgXI8nS902iwFjV1qhxVsC8ZAOUc77Kb4VpsPYylCOXVGc8P8zevYrulnoBLhsNbkcerDh8yV5vSMvRLcUbOn65jR10KNh3kf95cJ6NQed6oMQweVLZADUktgSrvVY6DSt801ZBJZAqUZD")
+        .setOAuthPermissions("email,publish_stream,publish_actions");
+    FacebookFactory ff = new FacebookFactory(cb.build());
+    facebook = ff.getInstance();
     
     }
+       
+    /**
+     * Publica un estado nuevo que introducimos por teclado al ejeutar el programa
+     * @param mensaje
+     */
+    
+    public void cambEstado(String mensaje){//Cambiar el estado usando permisos de visibilidad
+        try {
+            facebook.postStatusMessage(mensaje);
+        } catch (FacebookException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    /**
+     * Salta el mensaje que "Dale al boton de Like en la Pagina de Facebook fisicamente" por no funcionar automaticamente
+     */
     
     public void darGusta(){ //Darle me gusta a un post
-        
+        JOptionPane.showMessageDialog(null,"Dale al boton de Like en la Pagina de Facebook fisicamente");
     }
     
-    public void pubImagen(){//Post con imagen
+    /**
+     * Publica una imagen en nuestra Biografia por URL
+     * @throws MalformedURLException
+     */
+    
+    public void pubImagenURL()throws MalformedURLException{ //Postear una imagen por URL
+        try {
+            PostUpdate post = new PostUpdate(new URL("https://i.ytimg.com"))
+                    .picture(new URL("https://i.ytimg.com/vi/I2siTsRov48/maxresdefault.jpg"))
+                    .name("Rick")
+                    .caption("i.ytimg.com")
+                    .description("Rick & Morty");
+            facebook.postFeed(post);
+        } catch (FacebookException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
         
+    
+    /**
+     * Busca un post en nuestra biografia con los datos que le introducimos por teclado
+     * @throws FacebookException
+     */
+    
+    public void buscarPost() throws FacebookException{ //Busca un tema en la linea de tiempo
+        ResponseList<Post> results = facebook.getPosts("reboot");
+        System.out.println(results.toString());
     }
     
-    public void lineaTiempo(){ //Busca un tema en la linea de tiempo
-        
-    }
-    
-    public void comentariosPublicacion(){ //Comentarios de una publicación
-        
-    }
+    /**
+     * Publica un comentario que introducimos por teclado
+     */
     
     public void comentar(){ //Publicar un comentario
+        try {
+            facebook.commentPost(results.get(1).getId(), "hola holita");
+        } catch (FacebookException ex) {
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+   /**
+     * Muestra las publicaciones de nuestra biografia y Muestrar los Comentarios de una publicación
+     */
+   
+    public void mirarBiografia(){ //mirar biografia y comentarios
+        try{
+            ResponseList<Post> results= facebook.getFeed();
+            System.out.println(results);
+        } catch (FacebookException ex){
+            Logger.getLogger(Metodos.class.getName()).log(Level.SEVERE,null,ex);
+        }
+    }
+    
+    /**
+     * Salta el mensaje que "Fuera de Servicio. Facebook lo cerraria el servicio" por no funcionar automaticamente
+     */
+    
+    public void subirImagen(){ //subir imagen
         
+        JOptionPane.showMessageDialog(null,"Fuera de Servicio. Facebook lo cerraria el servicio");
     }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
 }
